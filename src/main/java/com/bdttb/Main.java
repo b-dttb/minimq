e8e186c50f99a014cf169a7212d28db38ecd4f95
b-dttb <b-dttbecome@outlook.com>
2024-04-01 20:56:36 +0800

    feat:test

---
+++ src/main/java/com/bdttb/Main.java
@@ -0,0 +1,15 @@
+package com.bdttb;
+
+/**
+ * @Description
+ * @Date 4/1/24 8:55 PM
+ * @Author <a href="mailto:b-dttbecome@outlook.com">b-dttb</a>
+ * @Blog <a href="https://b-dttb.github.io">https://b-dttb.github.io</a>
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
